83c4de215c8dfd9bfd7500901369637c5648a0f1
ShotaGhoona <shota.yamashita@ghoona.com>
2025-06-25 19:35:29 +0900

    許可されたオリジンにVercelのURLを追加し、CORS設定を更新しました。

--- backend/main.py
+++ backend/main.py
@@ -11,7 +11,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000",
+        "https://shintairiku-ig-4.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
